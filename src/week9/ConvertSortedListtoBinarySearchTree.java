fe718f85ddd65b13f182e87c0f5d09dbd68caecc
daisyonly <1241945585@qq.com>
2015-03-21 22:47:26 +0800

    --

--- src/week9/ConvertSortedListtoBinarySearchTree.java
+++ src/week9/ConvertSortedListtoBinarySearchTree.java
@@ -1,6 +1,14 @@
 package week9;
 
+import week3.ListNode;
+import week7.TreeNode;
+
 //https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
 public class ConvertSortedListtoBinarySearchTree {
+	
+   public TreeNode sortedListToBST(ListNode head) {
+	return null;
+        
+    }
 
 }
--- src/week9/MinimumDepthofBinaryTree.java
+++ src/week9/MinimumDepthofBinaryTree.java
@@ -1,6 +1,30 @@
 package week9;
 
+import week7.TreeNode;
+
 //https://leetcode.com/problems/minimum-depth-of-binary-tree/
 public class MinimumDepthofBinaryTree {
+	
+	private int minDepth = Integer.MAX_VALUE;
+    public int minDepth(TreeNode root) {
+    	if(root == null) return 0;
+		dfs(root,1);   	
+    	return minDepth;        
+    }
+	private void dfs(TreeNode node,int deep) {
+		if(node == null){
+			return;
+		}
+		if(node.left == null && node.right== null){
+			minDepth = deep < minDepth?   deep : minDepth;
+		}
+		dfs(node.left, deep+1);
+		dfs(node.right, deep+1);		
+	}
+	public static void main(String[] args){
+		MinimumDepthofBinaryTree  a = new MinimumDepthofBinaryTree();
+		TreeNode root = TreeNode.deserializeTree("{1,#,2}");
+		System.out.println(a.minDepth(root));
+	}
 
 }
