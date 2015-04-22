7d15bfedda71c49b02219b6c443c53071da24b7b
daisyonly <1241945585@qq.com>
2015-04-22 10:56:23 +0800

    11

--- src/main/java/week13/ScrambleString.java
+++ src/main/java/week13/ScrambleString.java
@@ -13,12 +13,30 @@ public class ScrambleString {
         Arrays.sort(c1);
         Arrays.sort(c2);
         if(!Arrays.equals(c1, c2)) return false;
-        for(int i=1; i<s1.length(); i++)
-        {
-            if(isScramble(s1.substring(0,i), s2.substring(0,i)) && isScramble(s1.substring(i), s2.substring(i))) return true;
-            if(isScramble(s1.substring(0,i), s2.substring(s2.length()-i)) && isScramble(s1.substring(i), s2.substring(0, s2.length()-i))) return true;
+        for(int i=1; i<s1.length(); i++){
+            if(isScramble(s1.substring(0,i), s2.substring(0,i)) && 
+            		isScramble(s1.substring(i), s2.substring(i))) 
+            	return true;
+            if(isScramble(s1.substring(0,i), s2.substring(s2.length()-i)) && 
+            		isScramble(s1.substring(i), s2.substring(0, s2.length()-i))) 
+            	return true;
         }
         return false;
     }
+	public boolean isScramble2(String s1, String s2) {
+	        int len=s1.length();
+	        boolean dp[][][] = new boolean[100][100][100];
+	        for (int i=len-1;i>=0;i--)
+	            for (int j=len-1;j>=0;j--) {
+	                dp[i][j][1]=(s1.charAt(i)==s2.charAt(j));
+	                for (int l=2;i+l<=len && j+l<=len;l++) {
+	                    for (int n=1;n<l;n++) {
+	                        dp[i][j][l]|=dp[i][j][n]&&dp[i+n][j+n][l-n];
+	                        dp[i][j][l]|=dp[i][j+l-n][n]&&dp[i+n][j][l-n];
+	                    }
+	                }
+	            }
+	        return dp[0][0][len];
+	  }
 
 }
