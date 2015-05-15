76932553c64bca2fe7cb027a93f2ab01877e1922
daisyonly <1241945585@qq.com>
2015-05-15 21:32:05 +0800

    11

--- src/main/java/fre3/FindMinimuminRotatedSortedArray.java
+++ src/main/java/fre3/FindMinimuminRotatedSortedArray.java
@@ -17,10 +17,23 @@ public class FindMinimuminRotatedSortedArray {
 		}
 		return min;      
     }
+	public int findMin2(int[] nums) {
+		if(nums==null||nums.length==0) return 0;
+		int left =0,right=nums.length-1;
+		while(left<right){
+			int mid = (left+right)>>1;
+		    if(nums[mid]>nums[right]){
+		    	left=mid+1;
+		    }else{
+		    	right=mid;
+		    }
+		}
+		return nums[left];      
+    }
 	public static void main(String[] args) {
 		int nums[]={4,5,6,7,0,1,2};
 		FindMinimuminRotatedSortedArray array= new FindMinimuminRotatedSortedArray();
-		System.out.println(array.findMin(nums));
+		System.out.println(array.findMin2(nums));
 	}
 
 }
