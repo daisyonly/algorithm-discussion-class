dd973abd29056630264d8f89195feb6f71c26748
daisyonly <1241945585@qq.com>
2015-05-29 17:20:38 +0800

    11

--- src/main/java/wap/Main.java
+++ src/main/java/wap/Main.java
@@ -1,38 +1,51 @@
 package wap;
 
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner cin = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 		int n,m,a,b;
-	    n = cin.nextInt();
-        m = cin.nextInt();
-		int edge[][] = new int[n+1][n+1];
+		in.nextToken();
+        n = (int)in.nval;
+        in.nextToken();
+        m = (int)in.nval;
+        @SuppressWarnings("unchecked")
+		LinkedList<Integer> edge[] = new LinkedList[n+1];
         Queue<Integer> que = new LinkedList<Integer>();
         int d[] = new int[n+1];
-        for(int i=1;i<n;i++){
-        	a = cin.nextInt();
-            b = cin.nextInt();
-        	edge[a][b]=1;
-        	//edge[b].add(a);
+        for(int i=1;i<=n;i++){
+        	edge[i] = new LinkedList<Integer>();
         	d[i]=n;
+        }
+        for(int i=1;i<n;i++){
+        	in.nextToken();
+            a = (int)in.nval;
+            in.nextToken();
+            b = (int)in.nval;
+            //out.println("\ntest:"+a+" "+b);
+        	edge[b].add(a);
+        	edge[a].add(b);
         }
         d[1] =0;
         que.add(1);
         while((m--)!=0){
-        	a = cin.nextInt();
-            b = cin.nextInt();
+        	in.nextToken();
+            a = (int)in.nval;
+            in.nextToken();
+            b = (int)in.nval;
             if(a==1){
             	d[b]=0;
             	que.add(b);
             }else{
             	bfs(edge,que,d);
-            	System.out.println(d[b]);
+            	out.println(d[b]);
             }
         }
+        out.flush();
 	}
 
 	private static void bfs(LinkedList<Integer>[] edge, Queue<Integer> que,
@@ -47,5 +60,4 @@ public class Main {
 			}
 		}	
 	}
-
 }
