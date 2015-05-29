e8dfd53befe3c3b5e815c727556f928f54a6ae9c
daisyonly <1241945585@qq.com>
2015-05-29 23:54:50 +0800

    11

--- src/main/java/wap/Game.java
+++ src/main/java/wap/Game.java
@@ -3,12 +3,12 @@ package wap;
 import java.util.Scanner;
 
 public class Game {
-	public static final int NoC = -2;
+	public static final long NoC = -2;
 	public static long map[][];
 	public static long dp[][][];
+	public static int n,m;
 	
 	public static void main(String[] args) {
-		int n,m,num;
 		Scanner cin = new Scanner(System.in);
 		n = cin.nextInt();
 		m = cin.nextInt();
@@ -21,41 +21,48 @@ public class Game {
 				 dp[i][j][1] = NoC;
 			}
 		}
-		dfs(1,1,0);
-		/*
+		cin.close();
+		long ans =-1;
 		for(int i=1;i<=n;i++){
-			for(int j=1;j<=m;j++){
-				for(int k=0;k<2;k++){
-					//dp[i][j][k]
-				}
-				dp[i][j][0]=Math.min(dp[i][j-1][0], dp[i][j-1][1]);
-				dp[i][j][0]=Math.min(dp[i][j][0], dp[i+1][j][0]);
-				dp[i][j][0] = dp[i][j][0]+ map[i][j];
-				
-				dp[i][j][1]=Math.min(dp[i][j-1][0], dp[i][j-1][1]);
-				dp[i][j][1]=Math.min(dp[i][j][0], dp[i+1][j][0]);
-			}
+			long tmp = dfs(i,m, 0);
+			ans = Math.max(ans, tmp);
+			tmp = dfs(i,m, 1);
+			ans = Math.max(ans, tmp);
 		}
-		*/
+		for(int i=0;i<=n;i++){
+			for (int j=0; j<=m; j++) {
+				System.out.print(dp[i][j][0]+"+"+dp[i][j][0]+"   ");
+			}
+			System.out.println();
+		}
+		System.out.println(ans);
 	}
 
 	private static long dfs(int i, int j, int k) {
 		// TODO Auto-generated method stub
 		if(dp[i][j][k] != NoC) return dp[i][j][k];
-		if(map[i][j]==-1){
+		if(map[i][j] == -1){
 			dp[i][j][k] = -1;
 			return dp[i][j][k];
 		}
-		dp[i][j][k] = Integer.MAX_VALUE; 
 		long num = dfs(i,j-1,k);
-		if(num != -1) dp[i][j][k]= Math.min(num, dp[i][j][k]);
-		num = dfs(i,j-1,k);
-		dp[i][j][k] = Math.min(num, dp[i][j][k]);
-		if(k==0){		
-			dp[i][j][k] = Math.min(dp[i][j][k], dfs(i+1,j,k));	
+		if(num != -1) dp[i][j][k] = Math.max(num, dp[i][j][k]);
+		if(k==0){
+			if(i < n){
+				num = dfs(i+1,j,k);
+				if(num != -1) dp[i][j][k] = Math.max(dp[i][j][k],num);	
+			}else{
+				dp[i][j][k] = Math.max(dp[i][j][k],0);	
+			}			
 		}else{
-			dp[i][j][k]=Math.min(dp[i][j][k], dfs(i-1,j,k));
+			if(i>1){
+				num = dfs(i-1,j,k);
+				if(num != -1) dp[i][j][k] = Math.max(dp[i][j][k], num);
+			}else{
+				dp[i][j][k] = Math.max(dp[i][j][k], 0);	
+			}
 		}
-		return dp[i][j][k]+map[i][j];	
+		if(dp[i][j][k]==-1) return -1;
+		return dp[i][j][k] + map[i][j];	
 	}
 }
